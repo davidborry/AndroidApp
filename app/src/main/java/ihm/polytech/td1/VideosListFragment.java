9f8eb2b857413e3deb04ce6b177565dd5e892abd
Günther Jungbluth <gunther@gameslabs.net>
2016-04-19 14:07:09 +0200

    Ma partie pédé

---
+++ app/src/main/java/ihm/polytech/td1/VideosListFragment.java
@@ -0,0 +1,67 @@
+package ihm.polytech.td1;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
+
+import java.util.List;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * to handle interaction events.
+ * Use the {@link VideosListFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class VideosListFragment extends Fragment {
+
+    public VideosListFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        new AsyncTask<String, Void, List<Video>>() {
+            @Override
+            protected List<Video> doInBackground(String... params) {
+                return new YoutubeAPI("UUC0wTzkzyLDrJt1NjeIT3Yg").getVideos();
+            }
+
+            @Override
+            protected void onPostExecute(List<Video> vids) {
+                final VideosCustomAdapter videos = new VideosCustomAdapter(getActivity(), R.id.news_empty);
+                videos.addAll(vids);
+                ((GridView) getActivity().findViewById(R.id.news_grid_layout)).setAdapter(videos);
+                ((GridView) getActivity().findViewById(R.id.news_grid_layout)).setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        Video video = (Video) parent.getItemAtPosition(position);
+                        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v="+video.getVideoId()));
+                                startActivity(browserIntent);
+                    }
+                });
+            }
+        }.execute();
+    }
+
+    public static VideosListFragment newInstance() {
+        VideosListFragment fragment = new VideosListFragment();
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_videos_list, container, false);
+    }
+
+}
